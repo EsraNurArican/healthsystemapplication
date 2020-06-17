e2cf0b53b8e16aaffa7412a2f04f362009844070
Esra Nur Arıcan <63196356+EsraNurArican@users.noreply.github.com>
2020-06-17 23:33:40 +0300

    Update PersonalData.java

--- src/PersonalData.java
+++ src/PersonalData.java
@@ -1,5 +1,55 @@
+/**
+ * PersonalData class represents personal information such as name, surname.
+ * Includes methods to change personal information.
+ */
 public class PersonalData {
+
     private String name;
-    private String surName;
+    private String surname;
     private int ID;
+
+    /**
+     * Changes person's name with a new name
+     * @param newName String new name parameter
+     * @return changed name
+     */
+    public String changeName(String newName){
+        name = newName;
+        return name;
+    }
+
+    /**
+     * Changes person's surname with a new surname
+     * @param newSurname String new surname parameter
+     * @return changed surname
+     */
+    public String changeSurname(String newSurname){
+        surname = newSurname;
+        return surname;
+    }
+
+    /**
+     * Getter for ID information
+     * @return int ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * Getter for name information
+     * @return String name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getter for surname information
+     * @return String surname
+     */
+    public String getSurname() {
+        return surname;
+    }
 }
+
