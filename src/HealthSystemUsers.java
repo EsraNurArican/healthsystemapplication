f95138b01bc630b39ab401782805a27f164fc874
Esra Nur Arıcan <63196356+EsraNurArican@users.noreply.github.com>
2020-06-17 23:33:03 +0300

    Update HealthSystemUsers.java

--- src/HealthSystemUsers.java
+++ src/HealthSystemUsers.java
@@ -1,3 +1,9 @@
+/**
+ * User interface for Health System Application
+ */
 public interface HealthSystemUsers {
+    public Boolean verifyUser(String name,String verifyPassword);
+    public  PersonalData getPersonalData();
 
 }
+
