6a7e7df05d568db7c553511424db217cc2caaaa3
Dilara Karakaş <60318526+ddilarakarakas@users.noreply.github.com>
2020-06-19 19:13:08 +0300

    Update MedicalData.java

--- src/MedicalData.java
+++ src/MedicalData.java
@@ -7,4 +7,34 @@ public class MedicalData {
     private ArrayList<String> notes;
     private ArrayList<String> operations;
     private Stack<Prescription> prescriptions;
+
+    public MedicalData(){
+        tests = new ArrayList<>();
+        notes = new ArrayList<>();
+        operations = new ArrayList<>();
+        prescriptions = new Stack<>();
+    }
+    public MedicalData(String test, String bloodType, String note, String operation, Prescription prescription){
+        tests = new ArrayList<>();
+        notes = new ArrayList<>();
+        operations = new ArrayList<>();
+        prescriptions = new Stack<>();
+        tests.add(test);
+        this.bloodType = bloodType;
+        notes.add(note);
+        operations.add(operation);
+        prescriptions.add(prescription);
+    }
+    public Stack<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+    public void addPrescription(Prescription prescription){
+        prescriptions.add(prescription);
+    }
+    public void addOperations(String operation){
+        operations.add(operation);
+    }
+    public void addTest(String test){
+        tests.add(test);
+    }
 }
