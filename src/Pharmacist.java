5ba386d2b34232573909b0a42d14e8849aab695e
Dilara Karakaş <60318526+ddilarakarakas@users.noreply.github.com>
2020-06-23 23:13:27 +0300

    Update Pharmacist.java

--- src/Pharmacist.java
+++ src/Pharmacist.java
@@ -1,5 +1,6 @@
 public class Pharmacist extends User {
     static int currentID=0;
+    private String pharmacyName;
     /**
      * Creates a person with given information.
      * @param personalData personal data of user
@@ -7,13 +8,23 @@ public class Pharmacist extends User {
      * @param password     password of user
      * @param hospital     the hospital to which the user belongs
      */
-    public Pharmacist(PersonalData personalData, String loginName, String password, Hospital hospital) {
+    public Pharmacist(PersonalData personalData, String loginName, String password, Hospital hospital,String pharmacyName) {
         super(personalData, loginName, password, hospital);
+        this.pharmacyName = pharmacyName;
     }
 
+    public void setPharmacyName(String pharmacyName) {
+        this.pharmacyName = pharmacyName;
+    }
+    public String getPharmacyName(){
+        return this.pharmacyName;
+    }
     static int getNextID(){
         currentID++;
         return currentID-1;
     }
-
+    @Override
+    public String toString() {
+        return "pharmacyName : " + pharmacyName + '\n';
+    }
 }
